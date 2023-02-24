c8fc225eb298bd962c89aced4bad65fbc5d32b24
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-02-24 14:11:20 +0200

    remove hook

--- src/models/User.ts
+++ src/models/User.ts
@@ -15,10 +15,6 @@ userSchema.pre('save', function (next) {
     });
 });
 
-userSchema.method('validatePassword', function (password) {
-    return bcrypt.compare(password, this.password);
-});
-
 type User = InferSchemaType<typeof userSchema>;
 
 export default model<User>("User", userSchema);
